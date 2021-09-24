5c054e13b31957ed0a8274c9eba871c35448d537
ceppetto <pcy07111@gmail.com>
2021-09-24 23:53:08 +0900

    Download images

--- selenium/google.py
+++ selenium/google.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import urllib.request
 
 driver = webdriver.Chrome()
 driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
@@ -8,9 +9,13 @@ driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
 elem = driver.find_element_by_name("q")
 elem.send_keys("dortmund")
 elem.send_keys(Keys.RETURN)
+
 driver.find_elements_by_css_selector(".rg_i.Q4LuWd")[0].click()
 time.sleep(3)
-print(driver.find_element_by_css_selector(".n3VNCb").get_attribute("src"))
+imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
+
+urllib.request.urlretrieve(imgUrl, "test.jpg")
+
 
 # assert "Python" in driver.title
 # elem = driver.find_element_by_name("q")
