0454b8e097ed8f07187dee1e139369b344463193
ceppetto <pcy07111@gmail.com>
2021-09-28 14:38:03 +0900

    Complete to make a crawler

--- selenium/google.py
+++ selenium/google.py
@@ -10,21 +10,36 @@ elem = driver.find_element_by_name("q")
 elem.send_keys("dortmund")
 elem.send_keys(Keys.RETURN)
 
-images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+# Scroll down to download all images
+# SCROLL_PAUSE_TIME = 1
+
+# last_height = driver.execute_script("return document.body.scrollHeight")
+
+# while True:
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#     time.sleep(SCROLL_PAUSE_TIME)
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+
+#     if new_height == last_height:
+#         try:
+#             driver.find_element_by_css_selector(".mye4qd").click()
+#         except:
+#             break
+
+#     last_height = new_height
+
+images = driver.find_elements_by_xpath(
+    "/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div[2]/div/a/img")
 count = 1
 for image in images:
-    image.click()
-    time.sleep(3)
-    imgUrl = driver.find_element_by_css_selector(
-        ".n3VNCb").get_attribute("src")
-    urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
-    count += 1
+    try:
+        image.click()
+        time.sleep(3)
+        imgUrl = driver.find_element_by_css_selector(
+            ".n3VNCb").get_attribute("src")
+        urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+        count += 1
+    except:
+        pass
 
-
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
+driver.close()
